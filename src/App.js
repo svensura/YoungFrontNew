acfd167b28816b1bab744e70e99cfe710e1d89c3
Sven Sura <svensura@germanscreens.de>
2023-04-29 11:45:53 +0200

    ..

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ class App extends Component {
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <Link to={"/"} className="navbar-brand">
-            CitModTool
+            TolScreens
           </Link>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
